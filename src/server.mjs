d0afa14a379add1916b1b972347dfffb7060a1cd
Abel Junior <abel.costa@webjump.com.br>
2022-05-26 17:57:39 -0300

    [Chore]: ading package
    Adding package: casual, to generate fake data

---
+++ src/server.mjs
@@ -0,0 +1,37 @@
+import http from 'node:http';
+import { Readable } from 'node:stream';
+import { randomUUID } from 'node:crypto';
+import casual from 'casual';
+
+// generator function
+function * run() {
+  for (let index = 0; index <= 999; index += 1) {
+    const data = {
+      id: randomUUID(),
+      name: `${casual.full_name}`,
+      email: `${casual.email}`,
+      at: Date.now()
+    }
+    yield data
+  }
+}
+
+function handler(request, response) {
+  let readableStream = Readable({
+    read() {
+      for (let data of run()) {
+        this.push(JSON.stringify(data).concat('\n'))
+      }
+
+      this.push(null);
+    }
+  });
+
+  readableStream.pipe(response);
+}
+
+let server = http.createServer(handler);
+
+server.listen(8080);
+
+server.on('listening', () => console.log(`Server is running on port 8080`));
